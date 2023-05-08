266c9b8a7514770805a394b3e00ead613bf4ff99
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-05-08 19:51:39 +0530

    Add on

---
+++ src/UpdateUser.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+import model.UserDao;
+
+
+@WebServlet("/update")
+public class UpdateUser extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+
+		
+		String name = request.getParameter("name");
+		String mail = request.getParameter("email");
+		String number = request.getParameter("number");
+		String pass = request.getParameter("pass");
+
+		User u =new User(name,mail,number,pass);
+		
+		
+		try {
+			int a= new UserDao().update(u);
+			if(a>0){
+				out.print("Updated!");	
+				response.sendRedirect("user");
+			}
+			else{
+				out.print("Not Updated!");
+				request.getRequestDispatcher("user").include(request, response);
+			}
+		} catch (ClassNotFoundException | SQLException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
